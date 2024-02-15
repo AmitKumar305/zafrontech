6fa0cdeababfa77439b3bb7085466e3f42facc44
Amit Kumar <65401586+AmitKumar305@users.noreply.github.com>
2024-02-15 17:35:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,12 @@ const express = require('express');
 const cors = require('cors');
 const connectDB = require('./db/index');
 const app = express();
-app.use(cors());
+app.use(cors({
+	origin: '*',
+	methods: ['GET', 'POST'],
+	allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'X-HTTP-Method-Override', 'Accept'],
+	credentials: true,
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(busboyBodyParser());
